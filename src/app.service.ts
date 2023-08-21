d55ab0ab44aeed3b897d4559233b491aa788ad7e
tais <tais15oliva@gmail.com>
2023-08-21 13:30:36 -0300

    feat/tweet POST

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,42 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { UserDTO } from './dtos/user.dtos';
+import { Tweet } from './entities/tweet.entity';
+import { TweetDTO } from './dtos/tweet.dtos';
 
 @Injectable()
 export class AppService {
 
   private users: User[]
+  private tweets: Tweet[]
 
   constructor(){
     this.users = []
+    this.tweets = []
   }
+
   
   signUP(body: UserDTO){
     const user = new User(body.username, body.avatar)
     return this.users.push(user)
+  }
+
+  getHealth() :string {
+    return "I'm okay!"
+  }
+
+  findUserByUsername(username: string): User | undefined {
+    return this.users.find(user => user.getUsername() === username);
+  }
+
+  postTweet(body: TweetDTO){
+    const checkUsername = this.findUserByUsername(body.username)
+    if(checkUsername === undefined){
+      throw new Error('UNAUTHORIZED')
+    }
+
+    const tweet = new Tweet(checkUsername, body.tweet)
+    return this.tweets.push(tweet)
+    
   }
 }
